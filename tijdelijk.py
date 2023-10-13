d8643fa5724655610d387ef6f23c632f653a23eb
yannickdegroot <yjcdegroot@hotmail.com>
2023-10-13 17:29:48 +0200

    functies toegevoegd

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,15 +1,27 @@
-prijzen = {
-    "aardbei" : 3,
-    "vanille" : 4,
-    "chocolade" : 5
-}
+from helper import decoreer
 
-aanbieding = prijzen["aardbei"] * 0.8
+def print_aanbieding():
+    prijzen = {
+        "aardbei" : 3,
+        "vanille" : 4,
+        "chocolade" : 5
+    }
 
-reclame_tekst = f"Vandaag in de aanbieding: aardbei-ijs, 1 liter - slechts € {aanbieding}"
-reclame_tekst2 = reclame_tekst[:-14]
-reclame_tekst3 = reclame_tekst2.upper()
-reclame_tekst4 = reclame_tekst3.split(" ")
+    aanbieding = prijzen["aardbei"] * 0.8
 
-for el in reclame_tekst4:
-    print(el.lower()) if len(el) <= 4 else print(el.upper())
+    reclame_tekst = f"Vandaag in de aanbieding: aardbei-ijs, 1 liter - slechts € {aanbieding}"
+
+    reclame_tekst2 = reclame_tekst[:-14]
+
+    reclame_tekst3 = reclame_tekst2.upper()
+
+    reclame_tekst4 = reclame_tekst3.split(" ")
+
+    for el in reclame_tekst4:
+        if len(el) > 4:
+            print(el.upper())
+        else:
+            print(el.lower())
+
+decoreer("aanbieding")
+print_aanbieding()
