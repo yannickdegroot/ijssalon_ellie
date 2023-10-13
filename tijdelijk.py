6e14d634f1535fc5552117e77049aaa280ccc2b5
yannickdegroot <yjcdegroot@hotmail.com>
2023-10-13 10:35:50 +0200

    bestand tijdelijk gecorrigeerd

--- tijdelijk.py
+++ tijdelijk.py
@@ -4,10 +4,11 @@ prijzen = {
     "chocolade" : 5
 }
 
-aanbieding = prijzen["vanille"] * 0.8
-reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € {aanbieding:.2f}"
+aanbieding = prijzen["aardbei"] * 0.8
 
-reclame_tekst3 = reclame_tekst.upper()
+reclame_tekst = f"Vandaag in de aanbieding: aardbei-ijs, 1 liter - slechts € {aanbieding}"
+reclame_tekst2 = reclame_tekst[:-14]
+reclame_tekst3 = reclame_tekst2.upper()
 reclame_tekst4 = reclame_tekst3.split(" ")
 
 for el in reclame_tekst4:
